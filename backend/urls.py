0f009b8652f6cd934ec604379c0e2812ccb2c37c
Truong <truongnguyen01653@gmail.com>
2025-09-19 16:18:02 +0700

    add

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('manage_app.urls')),
+    path('',include('manage_app.urls')),
 ]
